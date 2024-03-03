695f5e914793bdf1a6c0364b6062044716750413
b-go-bell <bridget.go.bell@gmail.com>
2024-03-03 04:36:20 -0800

    trips edits

--- app/src/pages/Trips.jsx
+++ app/src/pages/Trips.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './../resources/styles/components/Activity.css';
 import './../resources/styles/pages/Trips.css';
+import NavBar from './../components/NavBar.jsx';
 import { getTripsByUserId, getUserById, getImage } from '../firebase'
 
 function dateToString(timestamp) {
@@ -24,28 +25,28 @@ function Trips() {
           const invitedPromises = invited.map(async (trip) => {
             const organizerDetails = await getUserById(trip.organizer);
             const profileImage = await getImage("profile", trip.organizer);
-  
+
             return {
               ...trip,
               organizerDetails,
               profileImage,
             };
           });
-  
+
           const acceptedPromises = accepted.map(async (trip) => {
             const organizerDetails = await getUserById(trip.organizer);
             const profileImage = await getImage("profile", trip.organizer);
-  
+
             return {
               ...trip,
               organizerDetails,
               profileImage,
             };
           });
-  
+
           const invitedTripsWithData = await Promise.all(invitedPromises);
           const acceptedTripsWithData = await Promise.all(acceptedPromises);
-        
+
           setInvitedTrips(invitedTripsWithData);
           setAcceptedTrips(acceptedTripsWithData);
 
@@ -56,30 +57,33 @@ function Trips() {
     fetchUserData();
 }, []);
 
-  
+
   return (
-    <div>
-      <h1>Trips</h1>
-      <h2>Invitations</h2>
-      {/* create cards for each trip in invitedTrips here */}
-      {invitedTrips.map((trip, index) => (
-        <div className="card" key={index}>
-          <div />
-          <div className="card-text">
-            <div className="card-title">
-              {trip.name}
-            </div>
-            <div className="card-info">
-              <img src={trip.profileImage} className="profile-pic-thumbnail" alt="Profile Thumbnail" />
-              <p>{`${trip.organizerDetails.firstName} ${trip.organizerDetails.lastName}`}</p>
-              <p>+{trip.invitees.length - 1}</p>
-              <p>{dateToString(trip.date)}</p>
-              <button className="decline-button">decline</button>
-              <button className="accept-button">accept</button>
+    <div className="trips-container">
+      <NavBar />
+      <div className="trips-content" >
+      <h1 className="trips-h1">Trips</h1>
+      <h2 className="trips-h2">Invitations</h2>
+      <div className="cards-secton">
+        {invitedTrips.map((trip, index) => (
+          <div className="card" key={index}>
+            <div className="card-text">
+              <div className="card-title">
+                {trip.name}
+              </div>
+              <div className="card-info">
+                <img src={trip.profileImage} className="profile-pic-thumbnail" alt="Profile Thumbnail" />
+                <p>{`${trip.organizerDetails.firstName} ${trip.organizerDetails.lastName}`}</p>
+                <p>+{trip.invitees.length - 1}</p>
+                <p>{dateToString(trip.date)}</p>
+                <button className="decline-button">decline</button>
+                <button className="accept-button">accept</button>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+
 
       <h2>Accepted</h2>
       {/* create cards for each trip in acceptedTrips here */}
@@ -100,6 +104,8 @@ function Trips() {
       ))}
 
     </div>
+      </div>
+
   );
 }
 
