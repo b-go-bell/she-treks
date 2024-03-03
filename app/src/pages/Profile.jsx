9111926a16d661f7fb5eed53bd94645399259877
b-go-bell <bridget.go.bell@gmail.com>
2024-03-03 09:51:48 -0800

    profile

--- app/src/pages/Profile.jsx
+++ app/src/pages/Profile.jsx
@@ -6,7 +6,7 @@ import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import NavBar from './../components/NavBar';
 
 function Profile() {
-    const userId = "uFHGRa7deaMg9Azg9Aq5QKujMEJ2";
+    const userId = "zrFxCjWEybZoFiNGCOBYQBWDLri1";
 
     const [user, setUser] = useState();
     const [profileImageUrl, setProfileImageUrl] = useState();
@@ -102,11 +102,12 @@ function Profile() {
                                 {user ? `${user.location}` : 'Loading...'}
                             </div>
 
-                            <div className="profile-bio">
-                                {user ? `${user.bio}` : 'Loading...'}
-                            </div>
+
                         </div>
                     </div>
+                    <div className="profile-bio">
+                                {user ? `${user.bio}` : 'Loading...'}
+                            </div>
                 </div>
 
                 <div className='posts'>
--- app/src/pages/TripPage.jsx
+++ app/src/pages/TripPage.jsx
@@ -9,105 +9,19 @@ import { getTripsByUserId, getUserById, getImage } from '../firebase'
 function TripPage() {
   const userId = "zrFxCjWEybZoFiNGCOBYQBWDLri1";
 
-  const [invitedTrips, setInvitedTrips] = useState([]);
-  const [acceptedTrips, setAcceptedTrips] = useState([]);
-
-  useEffect(() => {
-    const fetchUserData = async () => {
-        try {
-          const invited = await getTripsByUserId("invitees", userId);
-          const accepted = await getTripsByUserId("accepted", userId);
-
-          const invitedPromises = invited.map(async (trip) => {
-            const organizerDetails = await getUserById(trip.organizer);
-            const profileImage = await getImage("profile", trip.organizer);
-
-            return {
-              ...trip,
-              organizerDetails,
-              profileImage,
-            };
-          });
-
-          const acceptedPromises = accepted.map(async (trip) => {
-            const organizerDetails = await getUserById(trip.organizer);
-            const profileImage = await getImage("profile", trip.organizer);
-
-            return {
-              ...trip,
-              organizerDetails,
-              profileImage,
-            };
-          });
-
-          const invitedTripsWithData = await Promise.all(invitedPromises);
-          const acceptedTripsWithData = await Promise.all(acceptedPromises);
-
-          setInvitedTrips(invitedTripsWithData);
-          setAcceptedTrips(acceptedTripsWithData);
-
-        } catch (error) {
-            console.error('Error fetching user data:', error.message);
-        }
-    };
-    fetchUserData();
-  }, []);
-
-  function getInvited() {
-    let invites = [];
-
-    if(invitedTrips.length === 0) {
-      invites.push(
-        <div className="no-trips">
-          No invited trips!
-        </div>
-      );
-    } else {
-      invitedTrips.map((t) => (
-        invites.push(
-        <div className="trips-list">
-          <Trip tri={t}/>
-        </div>
-      )));
-    }
-    return invites;
-  }
-
-  function getAccepted() {
-    let accepts = [];
-
-    if(acceptedTrips.length === 0) {
-      accepts.push(
-        <div className="no-trips">
-          No upcoming trips!
-        </div>
-      );
-    } else {
-      acceptedTrips.map((t) => (
-        accepts.push(
-        <div className="trips-list">
-          <Trip tri={t}/>
-        </div>
-      )));
-    }
-    return accepts;
-  }
-
-
 
   return (
     <div className="trips-container">
       <NavBar />
       <div className="trips-content" >
-      <h1 className="trips-h1">Trips</h1>
-      <h2 className="trips-h2">Trip Invitations</h2>
+      <h1 className="trips-h1">Trip</h1>
+      <h2 className="trips-h2"></h2>
       <div className="cards-section">
-        {getInvited()}
+
       </div>
 
       <h2 className="trips-h2">Upcoming Trips</h2>
       <div className="cards-section">
-        {getAccepted()}
       </div>
 
 
