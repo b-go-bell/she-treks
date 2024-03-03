a802a6472951c918c4258d9a3a8094ece3e051e2
Ariana Goldstein <arianang@usc.edu>
2024-03-02 18:30:11 -0800

    Add basic info and profile pic update to profile page

--- app/src/pages/Profile.jsx
+++ app/src/pages/Profile.jsx
@@ -1,15 +1,78 @@
+import React, { useEffect, useState } from 'react';
 import './../resources/styles/pages/App.css';
-import { getUsers } from '../firebase'
+import './../resources/styles/pages/Profile.css';
+import { getProfileImage, getUserById, setProfileImage } from '../firebase'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
-    getUsers();
+    const [user, setUser] = useState();
+    const [profileImageUrl, setProfileImageUrl] = useState(null);
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+          try {
+            // get user data from Firebase and Storage
+            const userData = await getUserById("Wv4ozXlaxEVRrgPYUvQ65YJAhyl1");
+            const userProfileImage = await getProfileImage("Wv4ozXlaxEVRrgPYUvQ65YJAhyl1");
+
+            setUser(userData);
+            setProfileImageUrl(userProfileImage);
+
+          } catch (error) {
+            console.error('Error fetching user data:', error.message);
+          }
+        };
+    
+        fetchUserData();
+      }, []);
+
+      const handleImageClick = () => {
+        document.getElementById('fileInput').click();
+      };
+    
+      const handleFileChange = async (e) => {
+        const file = e.target.files[0];
+    
+        if (file) {
+          try {
+            // Upload the profile image and get the download URL to update the image on the page
+            await setProfileImage("Wv4ozXlaxEVRrgPYUvQ65YJAhyl1", file);
+            const downloadUrl = await getProfileImage("Wv4ozXlaxEVRrgPYUvQ65YJAhyl1");
+            setProfileImageUrl(downloadUrl);
+
+          } catch (error) {
+            console.error('Error uploading profile image in Profile:', error.message);
+          }
+        }
+      };
+
     return (
-        <div className="App">
-        <header className="App-header">
-            <p>
-            Profile Page
-            </p>
-        </header>
+        <div>
+            <div className="circular-image-container" onClick={handleImageClick}>
+            <input
+                type="file"
+                id="fileInput"
+                accept="image/*"
+                style={{ display: 'none' }}
+                onChange={handleFileChange}
+            />
+            <img
+                className="circular-image"
+                src={profileImageUrl || 'https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg'}
+                alt="User Profile"
+            />
+            </div>
+
+            <h1>{user ? 
+                `${user.firstName + " " + user.lastName}` 
+                : 'Loading...'}
+            </h1>
+            <h3>
+                <FontAwesomeIcon icon={faLocationDot} />
+                {user ?
+                `${user.location}` : 'Loading...'}
+            </h3>
         </div>
     );
     }
